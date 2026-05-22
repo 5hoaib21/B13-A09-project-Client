eb18ff1b094df8669760956a6a21777c3697949b
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-05-22 17:52:22 +0600

    spinner showed on every route

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,13 @@
+"use client";
 import React from "react";
-import { Triangle } from "react-loader-spinner";
+
+import { DotLoader } from "react-spinners";
 
 const LoadingPage = () => {
   return (
-    <div className="flex h-[85vh] items-center justify-center">
-      <Triangle
+    <div className="flex flex-col space-y-8 h-[85vh] items-center justify-center">
+      <h2 className="text-3xl font-bold">StudyNook</h2>
+      <DotLoader
         visible={true}
         height="80"
         width="80"
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,6 @@ import { CircleChevronLeft } from "@gravity-ui/icons";
 import { Button } from "@heroui/react";
 import Link from "next/link";
 import React from "react";
-import { BsBack } from "react-icons/bs";
 
 const NotFound = () => {
   return (
